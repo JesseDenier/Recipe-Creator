f42ca58e66d8c9f267fe019454feca576ad37063
Sadie Thongsavanh <sadiethongsavanh@Sadies-MacBook-Air.local>
2024-02-02 12:34:31 -0600

    made button clickable and converted input to array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,10 @@
-// Saves the private API Key for spoonacular.
-const spoonacularAPIKey = "de07b56c97684defbae6bc906f8a5352";
-
-// Fetches data for recipes using the ingredient "apple."
-// TODO: Replace "apple" with an array of ingredients entered in by the end user.
-fetch(
-  "https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples&apiKey=" +
-    spoonacularAPIKey
-)
-  // Converts that data into a JSON file which we can parse.
-  .then(function (response) {
-    return response.json();
-  })
-  // Prints the JSON data to the console.
-  // TODO: Have the data create HTML elements instead of printing to the console.
-  .then(function (data) {
-    console.log(data);
-  });
+// TODO: Create a function that starts on search button click, and turns the user input into an array, which can be referenced by edamam and spoonacular js.
+let submitB = document.getElementById("recipeSearchBtn")
+let inputB = document.getElementById("ingredientInput")
+let arrayB = []
+submitB.addEventListener("click",function(){
+    
+    let inputValue = inputB.value
+    let inputValueB = inputValue.split(" ")
+    console.log("hello",inputValueB)
+})
