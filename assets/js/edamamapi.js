16ab195a3289567e34e2d6c090a2fd1744d58466
Jesse <jessecdenier@gmail.com>
2024-02-01 20:55:06 -0600

    Split the js into 3 files so there are less merge conflicts.

---
+++ assets/js/edamamapi.js
@@ -0,0 +1,21 @@
+// Saves the private API Key and API Id for edamam.
+const edamamAPIKey = "bcbb167d1766a0e612f1bb055f2ac680";
+const edamamAPIId = "dc99804d";
+
+// Fetches data for recipes using the ingredient "apple."
+// TODO: Replace "apple" with an array of ingredients entered in by the end user.
+fetch(
+  "https://api.edamam.com/api/recipes/v2?type=public&q=apple&app_id=" +
+    edamamAPIId +
+    "&app_key=" +
+    edamamAPIKey
+)
+  // Converts that data into a JSON file which we can parse.
+  .then(function (response) {
+    return response.json();
+  })
+  // Prints the JSON data to the console.
+  // TODO: Have the data create HTML elements instead of printing to the console.
+  .then(function (data) {
+    console.log(data);
+  });
