c19aee7c94d8ea41054aa480b02fc01abd80ab90
Jesse <jessecdenier@gmail.com>
2024-02-03 13:24:21 -0600

    Added correct parameters for Spoon API fetch, and built infastructure for ingredientArray to be inserted.

--- assets/js/spoonacularapi.js
+++ assets/js/spoonacularapi.js
@@ -1,10 +1,32 @@
 // Saves the private API Key for spoonacular.
 const spoonacularAPIKey = "de07b56c97684defbae6bc906f8a5352";
 
-// Fetches data for recipes using the ingredient "apple."
-// TODO: Replace "apple" with an array of ingredients entered in by the end user.
+// ! This is only here for testing purposes until ingredientArray is populated by script.js.
+const ingredientArray = [
+  "apple",
+  "banana",
+  "garlic",
+  "balsamic vinegar",
+  "kale",
+  "onion",
+  "bread",
+  "figs",
+  "goat cheese",
+  "pesto",
+];
+
+// Converts ingredientArray into a single string with the correct punctuation between each ingredient to be inserted as an API fetch parameter.
+const spoonIngredientString = ingredientArray.map(String).join(",+");
+
+// Fetches data for recipes within the following parameters:
+// List up to 100 recipes.
+// Order them by least amount of "missing ingredients."
+// Ignore pantry items like water, flour, salt in ingredient check.
+// Ingredients = What is in ingredientArray
 fetch(
-  "https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples&apiKey=" +
+  "https://api.spoonacular.com/recipes/findByIngredients?number=100&ranking=2&ignorePantry=true&ingredients=" +
+    spoonIngredientString +
+    "&apiKey=" +
     spoonacularAPIKey
 )
   // Converts that data into a JSON file which we can parse.
@@ -12,6 +34,7 @@ fetch(
     return response.json();
   })
   // Prints the JSON data to the console.
+  // TODO: Create a function that checks if there are any "missedIngredients" objects in the returned recipe and remove those recipes from the array.
   // TODO: Have the data create HTML elements instead of printing to the console.
   .then(function (data) {
     console.log(data);
