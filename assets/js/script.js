569ad3363e1b6c5056d3ab6433b2e25347c753cb
Jesse <jessecdenier@gmail.com>
2024-02-05 14:43:02 -0600

    Merged Spoon and Script API so they could call the same variables. Removed unneeded console log tests, and confirmed functions all work as intended and run in the correct order on search button click.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,51 @@
-// TODO: Create a function that starts on search button click, and turns the user input into an array, which can be referenced by edamam and spoonacular js.
+// References the button that had the ID recipeSearchBtn.
+const searchBtn = document.getElementById("recipeSearchBtn");
+// References the input that had the ID ingredientInput.
+const userIngredientInput = document.getElementById("ingredientInput");
+// Initializes an empty array.
+let ingredientArray = [];
 
-// Refrence the button that had the ID recipeSearchBtn
-let searchBtn = document.getElementById("recipeSearchBtn")
-// Refrence the input that had the ID ingredientInput
-let userIngredientInput = document.getElementById("ingredientInput")
-// Initialized an empty array
-let ingredientArray = []
+// Saves the private API Key for spoonacular.
+const spoonacularAPIKey = "de07b56c97684defbae6bc906f8a5352";
 
-// Attached a click event to searchBtn
-searchBtn.addEventListener("click",function(){
-    // Saved the value of the input to the variable inputValue
-    let inputValue = userIngredientInput.value
-    // Converted the string inputValue to an array; ingredientArray
-    ingredientArray = inputValue.split(", ")
-    console.log("hello",ingredientArray)
-})
+// Removes all recipes from Spoon JSON data that have more than 1 "missedIngredients."
+function spoonFilterRecipes(data) {
+  return data.filter(function (recipe) {
+    return recipe.missedIngredients.length <= 1;
+  });
+}
+
+// Take value created with the searchBtn click event and fetches the correct data.
+function spoonFetch() {
+  // Converts ingredientArray into a single string with the correct punctuation between each ingredient to be inserted as an API fetch parameter.
+  let spoonIngredientString = ingredientArray.map(String).join(",+");
+  // Fetches data for recipes within the following parameters:
+  // List up to 100 recipes.
+  // Order them by least amount of "missing ingredients."
+  // Ignore pantry items like water, flour, salt in ingredient check.
+  // Ingredients = What is in ingredientArray
+  fetch(
+    "https://api.spoonacular.com/recipes/findByIngredients?number=100&ranking=2&ignorePantry=true&ingredients=" +
+      spoonIngredientString +
+      "&apiKey=" +
+      spoonacularAPIKey
+  )
+    // Converts that data into a JSON file which we can parse.
+    .then(function (response) {
+      return response.json();
+    })
+    // Prints the JSON data to the console after calling the "spoonFilterRecipes" function.
+    // TODO: Have the data create HTML elements instead of printing to the console.
+    .then(function (data) {
+      let spoonFilteredRecipes = spoonFilterRecipes(data);
+      console.log(spoonFilteredRecipes);
+    });
+}
+
+// Attaches a click event to searchBtn.
+searchBtn.addEventListener("click", function () {
+  // Fills the empty ingredientArray with the users input.
+  ingredientArray = userIngredientInput.value.split(", ");
+  // Calls the spoonFetch function.
+  spoonFetch();
+});
