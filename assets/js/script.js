849fde900640ff00d87ebb76df78e0073ab79cd6
Sadie Thongsavanh <sadiethongsavanh@Sadies-MacBook-Air.local>
2024-02-05 13:26:47 -0600

    updated variable names and added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,17 @@
 // TODO: Create a function that starts on search button click, and turns the user input into an array, which can be referenced by edamam and spoonacular js.
 
-let submitB = document.getElementById("recipeSearchBtn")
-let inputB = document.getElementById("ingredientInput")
-let arrayB = []
-submitB.addEventListener("click",function(){
-    
-    let inputValue = inputB.value
-    let inputValueB = inputValue.split(" ")
-    console.log("hello",inputValueB)
+// Refrence the button that had the ID recipeSearchBtn
+let searchBtn = document.getElementById("recipeSearchBtn")
+// Refrence the input that had the ID ingredientInput
+let userIngredientInput = document.getElementById("ingredientInput")
+// Initialized an empty array
+let ingredientArray = []
+
+// Attached a click event to searchBtn
+searchBtn.addEventListener("click",function(){
+    // Saved the value of the input to the variable inputValue
+    let inputValue = userIngredientInput.value
+    // Converted the string inputValue to an array; ingredientArray
+    ingredientArray = inputValue.split(", ")
+    console.log("hello",ingredientArray)
 })
