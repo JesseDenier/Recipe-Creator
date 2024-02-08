0ebd88f50adef34f2fba7cd787a601a8071f1f53
Jesse <jessecdenier@gmail.com>
2024-02-08 11:33:22 -0600

    Cleaned up notation on all code.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,50 @@
-// Links the HTML spoon input to the JS.
+// Links the HTML inputs and buttons to the JS.
 const spoonIngredientInput = document.getElementById("spoonIngredientInput");
-// Links the HTML spoon search button to the JS.
 const spoonSearchBtn = document.getElementById("spoonRecipeSearchBtn");
-// Links the HTML edamam input to the JS.
 const edamamIngredientInput = document.getElementById("edamamIngredientInput");
-// Links the HTML edamam search button to the JS.
 const edamamSearchBtn = document.getElementById("edamamRecipeSearchBtn");
 
 // Initializes empty arrays.
 let spoonIngredientArray = [];
 let edamamIngredientArray = [];
 
-// Saves the private API info
+// Saves personal API info.
 const spoonacularAPIKey = "b1e8d5af6d6f4efaaa60746bf1c9cd8c";
 const edamamAPIKey = "bcbb167d1766a0e612f1bb055f2ac680";
 const edamamAPIId = "dc99804d";
 
-// Removes all recipes from Spoon JSON data that have more than 1 "missedIngredients."
+// Removes all recipes from Spoon JSON data that have more than any missing ingredients.
 function spoonFilterRecipes(data) {
   return data.filter(function (recipe) {
     return recipe.missedIngredients.length <= 0;
   });
 }
 
-// Creates a recipe card based on spoonFetch data.
+// Creates recipe cards based on spoonFetch data.
 function spoonCreateCard(recipe) {
-  // Create a div element for the recipe card
+  // Create a div element for the recipe card.
   const card = document.createElement("div");
   card.classList.add("recipe-card");
-  // Create an h2 element for the recipe name
+  // Create an h2 element with the recipe name.
   const recipeName = document.createElement("h2");
-  recipeName.textContent = recipe.title; // not a name variable in JSON
-  // Create a ul element for the ingredients list
+  recipeName.textContent = recipe.title;
+  // Create a ul element for the ingredients list.
   const ingredientsList = document.createElement("ul");
-  // Iterate over the ingredients and create li elements for each
+  // Iterate over the ingredients and create li elements for each.
   recipe.usedIngredients.forEach((ingredient) => {
     const ingredientItem = document.createElement("li");
-    ingredientItem.textContent = ingredient.name; // Assuming the ingredient object has a 'name' property
+    ingredientItem.textContent = ingredient.name;
     ingredientsList.appendChild(ingredientItem);
   });
-  // Append the recipe name and ingredients list to the card
+  // Appends the recipe name and ingredients list to the card.
   card.appendChild(recipeName);
   card.appendChild(ingredientsList);
-  // Append the card to the document or a specific container
-  document.body.appendChild(card); // Adjust this line based on where you want to append the cards
+  // Append the card to the HTML.
+  // TODO: Create a space in the HTML for these cards to go and then change "body" to that space.
+  document.body.appendChild(card);
 }
 
-// Take value created with the spoonSearchBtn click event and fetches the correct data.
+// Takes the value created with the spoonSearchBtn click event and fetches the correct data.
 function spoonFetch() {
   // Converts ingredientArray into a single string with the correct punctuation between each ingredient to be inserted as an API fetch parameter.
   let spoonIngredientString = spoonIngredientArray.map(String).join(",+");
@@ -65,8 +63,7 @@ function spoonFetch() {
     .then(function (response) {
       return response.json();
     })
-    // Prints the JSON data to the console after calling the "spoonFilterRecipes" function.
-    // TODO: Have the data create HTML elements instead of printing to the console.
+    // Calls "spoonFilterRecipes," then console logs the result, and then calls "spoonCreateCard" for each recipe.
     .then(function (data) {
       let spoonFilteredRecipes = spoonFilterRecipes(data);
       console.log(spoonFilteredRecipes);
@@ -76,31 +73,37 @@ function spoonFetch() {
     });
 }
 
-// This function creates a recipe card
+// Creates recipe cards based on edamamFetch data.
 function edamamCreateCard(recipe) {
-  // Create a div element for the recipe card
+  // Create a div element for the recipe card.
   const card = document.createElement("div");
   card.classList.add("recipe-card");
-  // Create an h2 element for the recipe name
+  // Create an h2 element with the recipe name.
   const recipeName = document.createElement("h2");
-  recipeName.textContent = recipe.recipe.label; // not a name variable in JSON
-  // Create a ul element for the ingredients list
+  recipeName.textContent = recipe.recipe.label;
+  // Create a ul element for the ingredients list.
   const ingredientsList = document.createElement("ul");
-  // Iterate over the ingredients and create li elements for each
+  // Iterate over the ingredients and create li elements for each.
   recipe.recipe.ingredients.forEach((ingredient) => {
     const ingredientItem = document.createElement("li");
     ingredientItem.textContent = ingredient.food;
     ingredientsList.appendChild(ingredientItem);
   });
-  // Append the recipe name and ingredients list to the card
+  // Append the recipe name and ingredients list to the card.
   card.appendChild(recipeName);
   card.appendChild(ingredientsList);
-  // Append the card to the document or a specific container
-  document.body.appendChild(card); // Adjust this line based on where you want to append the cards
+  // Append the card to the HTML.
+  // TODO: Create a space in the HTML for these cards to go and then change "body" to that space.
+  document.body.appendChild(card);
 }
 
+// Takes the value created with the edamamSearchBtn click event and fetches the correct data.
 function edamamFetch() {
-  let edamamIngredientString = edamamIngredientArray.map(String).join("%2C%20");
+  // Converts ingredientArray into a single string to be inserted as an API fetch parameter.
+  let edamamIngredientString = edamamIngredientArray.map(String);
+  // Fetches data for recipes within the following parameters:
+  // Only find public recipes.
+  // Recipes need to involve the ingredient user inputed.
   fetch(
     "https://api.edamam.com/api/recipes/v2?type=public&q=" +
       edamamIngredientString +
@@ -113,12 +116,13 @@ function edamamFetch() {
     .then(function (response) {
       return response.json();
     })
+    //Console logs the result, and then calls "edamamCreateCard" for each recipe.
     .then(function (data) {
       let edamamFilteredRecipes = data.hits;
       console.log(edamamFilteredRecipes);
       edamamFilteredRecipes.forEach((recipe) => {
         edamamCreateCard(recipe);
-      }); // Close the forEach function
+      });
     });
 }
 
@@ -138,10 +142,10 @@ edamamSearchBtn.addEventListener("click", function () {
   edamamFetch();
 });
 
-//pop up window - Welcome message
-$(document).ready(function() {
-  $('#popup').show();
-  $('#closePopup').click(function() {
-    $('#popup').hide();
+// Creates an interactive welcome message on page load.
+$(document).ready(function () {
+  $("#popup").show();
+  $("#closePopup").click(function () {
+    $("#popup").hide();
   });
 });
